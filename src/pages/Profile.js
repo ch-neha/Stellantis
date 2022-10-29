1249a15d3f2a8d63116873db962f0c593928d7a3
Neha <23nehachekuri@gmail.com>
2022-10-30 04:11:00 +0530

    frontend

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import ProfileCard from '../components/ProfileCard';
 import ProfileChart from '../components/ProfileChart';
+import Badge from '../components/Badge/Badge';
 
 export default function Profile() {
+    const data = [
+        {
+          type: 1,
+          name: "A week of Safety",
+        },
+        {
+          type: 2,
+          name: "Travelled 5000km",
+        },
+        {
+          type: 3,
+          name: "Maintenance Excellence",
+        },
+        {
+          type: 4,
+          name: "Clean Freak",
+        },
+        {
+          type: 5,
+          name: "100 days on road",
+        },
+    ];
     return (
         <div>
             <ProfileCard/>
+            <Badge data={data} />
             <ProfileChart/>
         </div>
     )
