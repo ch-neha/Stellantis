012d4da2ea1d8a5453f44fb7dc32fd17433bde76
khssupriya <khssupriya@gmail.com>
2022-10-29 22:43:27 +0530

    separate coupons to a different section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import './App.css';
-import CircleProgress from './components/CircleProgress'
+import Coupons from './components/Coupons';
+import CircleProgressSection from './components/CircleProgressSection';
 
 function App() {
   return (
     <div className="App">
-      <div class="container">
-        <div class="row">
-          <div class="col-3">
-            <CircleProgress percentage={65} text={'safety'}/>
-          </div>
-          <div class="col-3">
-            <CircleProgress percentage={77} text={'health'}/>
-          </div>
-          <div class="col-3">
-            <CircleProgress percentage={33} text={'Environmental Friendly'}/>
-          </div>
-          <div class="col-3">
-            <CircleProgress percentage={26} text={'Daily Task'}/>
-          </div>
-        </div>
-      </div>
+      <CircleProgressSection/>
+      <Coupons/>
     </div>
   );
 }
