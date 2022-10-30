55db97634c323c9a28bd8d9b724e0280f7672a4d
drakcoder <sairamgundala2000@gmail.com>
2022-10-30 06:55:43 +0530

    fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -71,15 +71,6 @@ function Navbar() {
                 Quiz
               </NavLink>
             </li>
-            <li className="nav-item">
-              <NavLink
-                className="nav-link"
-                activeClassName="active text-primary"
-                to="/simulation"
-              >
-                Actions
-              </NavLink>
-            </li>
             <li className="nav-item mx-2">
               {user ? (
                 <div class="dropdown">
