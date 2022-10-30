49f60b3526528caed726630da23dc9a4e7684f05
Neha <23nehachekuri@gmail.com>
2022-10-30 10:40:47 +0530

    final

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Card } from 'react-bootstrap';
-import userImage from '../images/user.png';
-import { signInWithGoogle, auth, signOut } from "../services/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../services/auth";
+import { getUserbyId } from "../services/user";
 import CircleProgressSection from '../components/CircleProgressSection';
 
 export default function ProfileCard() {
@@ -13,17 +13,26 @@ export default function ProfileCard() {
     const [ranking, setranking] = useState('37');
     const [streak, setstreak] = useState('7');
     const [level, setlevel] = useState('Social Leader');
+
+    useEffect(() => {
+        if(user)
+            getUserbyId(user.uid).then(user_data => {
+                console.log(user_data)
+                setname(user_data);
+            })
+        }
+    , [user])
     return (
         <div style={{ padding: "40px" }}>
             <Card style={{ padding: "30px", }}>
                 <div style={{ display: "flex", justifyContent: "center" }}>
-                    <Card.Img variant="top" src={userImage} style={{ width: "300px", height: "280px", borderRadius: "9px" }} />
+                    <Card.Img variant="top" src={user.photoURL} style={{ width: "300px", height: "280px", borderRadius: "9px" }} />
                     <div style={{ width: "50px" }}></div>
                     <div>
-                        <Card.Title style={{ fontSize: "40px" }}>{name}</Card.Title>
+                        <Card.Title style={{ fontSize: "40px" }}>{name.name}</Card.Title>
                         <Card.Text>
                             <div style={{ height: "20px" }}></div>
-                            <p>Total Points Earned: <b>{points}</b></p>
+                            <p>Total Points Earned: <b>{name.points}</b></p>
                             <p>Current Position on Leader Board: <b>#{ranking}</b></p>
                         </Card.Text>
                         <div style={{ display: "flex", justifyContent: "center" }}>
--- src/pages/Leaderboard.js
+++ src/pages/Leaderboard.js
@@ -1,188 +1,178 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../services/auth";
+import { getUserbyId } from "../services/user";
+
 
 export default function LeaderBoard() {
+const [user] = useAuthState(auth);
+const [user_data, setdata] = useState()
   const [data, setData] = useState([
-    {
-      name: "Hanna",
-      location: "India",
-      score: 550,
-      img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-      dt: "2022-06-1",
-      streak: 10,
-      level: "Rookie",
-    },
-    {
-      name: "Shawn Mendes",
-      location: "India",
-      score: 440,
-      img: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-      dt: "2022-05-29",
-      streak: 7,
-      level: "Social Leader",
-    },
-    {
-      name: "Camila",
-      location: "India",
-      score: 350,
-      img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-      dt: "2022-05-30",
-      streak: 2,
-      level: "The Wise",
-    },
-    {
-      name: "Shawn Hanna",
-      location: "India",
-      score: 225,
-      img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-      dt: "2022-02-10",
-      streak: 6,
-      level: "The Wise",
-    },
-    {
-      name: "Fidel Hand",
-      location: "USA",
-      score: 100,
-      img: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-      dt: "2021-01-01",
-      streak: 1,
-      level: "Social Servent",
-    },
+        {
+          name: "Hanna",
+          location: "India",
+          points: 550,
+          img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+          dt: "2022-06-1",
+          streak: 10,
+          level: "Rookie",
+        },
+        {
+          name: "Shawn Mendes",
+          location: "India",
+          points: 440,
+          img: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+          dt: "2022-05-29",
+          streak: 7,
+          level: "Social Leader",
+        },
+        {
+          name: "Camila",
+          location: "India",
+          points: 350,
+          img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+          dt: "2022-05-30",
+          streak: 2,
+          level: "The Wise",
+        },
+        {
+          name: "Shawn Hanna",
+          location: "India",
+          points: 225,
+          img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+          dt: "2022-02-10",
+          streak: 6,
+          level: "The Wise",
+        },
+        {
+          name: "Fidel Hand",
+          location: "USA",
+          points: 100,
+          img: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+          dt: "2021-01-01",
+          streak: 1,
+          level: "Social Servent",
+        },
+        {
+          name: "Bessie Hickle",
+          location: "China",
+          points: 1350,
+          img: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+          dt: "2021-08-17",
+          streak: 10,
+          level: "Social Servent",
+        },
+        {
+          name: "Adella Wunsch",
+          location: "Japan",
+          points: 1100,
+          img: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+          dt: "2021-10-23",
+          streak: 10,
+          level: "Social Servent",
+        },
+        {
+          name: "Clair O'Connell",
+          location: "London",
+          points: 1050,
+          img: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+          dt: "2022-01-22",
+          streak: 10,
+          level: "The Wise",
+        },
+        {
+          name: "Kameron Prosacco",
+          location: "Canada",
+          points: 750,
+          img: "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+          dt: "2022-01-21",
+          level: "The Wise",
+          streak: 10,
+        },
+        {
+          name: "Fidel Hand",
+          location: "China",
+          points: 3350,
+          img: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+          dt: "2021-08-17",
+          streak: 10,
+          level: "Social Servent",
+        },
+        {
+          name: "Adella Wunsch",
+          location: "Japan",
+          points: 2100,
+          img: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+          dt: "2021-10-23",
+          streak: 10,
+          level: "Social Servent",
+        },
+        {
+          name: "Clair O'Connell",
+          location: "London",
+          points: 1950,
+          img: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+          dt: "2022-01-22",
+          streak: 10,
+          level: "The Wise",
+        },
+        {
+          name: "Kameron Prosacco",
+          location: "Canada",
+          points: 1750,
+          img: "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
+          dt: "2022-01-21",
+          level: "The Wise",
+          streak: 10,
+        },
   ]);
 
-  const [period, setPeriod] = useState(0);
+  const [period, setPeriod] = useState({start: 0, end: 600});
 
   const handleClick = (e) => {
-    setData([
-      {
-        name: "Hanna",
-        location: "India",
-        score: 550,
-        img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-        dt: "2022-06-1",
-        streak: 10,
-        level: "Rookie",
-      },
-      {
-        name: "Shawn Mendes",
-        location: "India",
-        score: 440,
-        img: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-        dt: "2022-05-29",
-        streak: 7,
-        level: "Social Leader",
-      },
-      {
-        name: "Camila",
-        location: "India",
-        score: 350,
-        img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-        dt: "2022-05-30",
-        streak: 2,
-        level: "The Wise",
-      },
-      {
-        name: "Shawn Hanna",
-        location: "India",
-        score: 225,
-        img: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-        dt: "2022-02-10",
-        streak: 6,
-        level: "The Wise",
-      },
-      {
-        name: "Fidel Hand",
-        location: "USA",
-        score: 100,
-        img: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-        dt: "2021-01-01",
-        streak: 1,
-        level: "Social Servent",
-      },
-    ]);
+    setPeriod({start: 0, end: 600})
   };
 
   const handleClick2 = (e) => {
-    setData([
-      {
-        name: "Bessie Hickle",
-        location: "China",
-        score: 1350,
-        img: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-        dt: "2021-08-17",
-        streak: 10,
-        level: "Social Servent",
-      },
-      {
-        name: "Adella Wunsch",
-        location: "Japan",
-        score: 1100,
-        img: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-        dt: "2021-10-23",
-        streak: 10,
-        level: "Social Servent",
-      },
-      {
-        name: "Clair O'Connell",
-        location: "London",
-        score: 1050,
-        img: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-        dt: "2022-01-22",
-        streak: 10,
-        level: "The Wise",
-      },
-      {
-        name: "Kameron Prosacco",
-        location: "Canada",
-        score: 750,
-        img: "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-        dt: "2022-01-21",
-        level: "The Wise",
-        streak: 10,
-      },
-    ]);
+    setPeriod({start: 600, end: 1500})
   };
 
   const handleClick3 = (e) => {
-    setData([
-      {
-        name: "Fidel Hand",
-        location: "China",
-        score: 3350,
-        img: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-        dt: "2021-08-17",
-        streak: 10,
-        level: "Social Servent",
-      },
-      {
-        name: "Adella Wunsch",
-        location: "Japan",
-        score: 2100,
-        img: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-        dt: "2021-10-23",
-        streak: 10,
-        level: "Social Servent",
-      },
-      {
-        name: "Clair O'Connell",
-        location: "London",
-        score: 1950,
-        img: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-        dt: "2022-01-22",
-        streak: 10,
-        level: "The Wise",
-      },
-      {
-        name: "Kameron Prosacco",
-        location: "Canada",
-        score: 1750,
-        img: "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60",
-        dt: "2022-01-21",
-        level: "The Wise",
-        streak: 10,
-      },
-    ]);
+    setPeriod({start: 1500, end: 3500})
   };
+
+  useEffect(() => {
+    if(user)
+        getUserbyId(user.uid).then(user_data => {
+            console.log(user_data)
+            if(data.filter(val => val && val.id).length) {
+                setData(data.map(val => {
+                    if(val && val.id && val.id === user_data.id)
+                        return {
+                            id: user_data.id,
+                            name: user_data.name,
+                            location: "India",
+                            points: user_data.points ? user_data.points: 0,
+                            img: user_data.photoURL,
+                            level: "The Wise",
+                            streak: 2,
+                        }
+                    return val;
+                }))
+            } else {
+            setData([...data, {id: user_data.id,
+
+                name: user_data.name,
+                location: "India",
+                points: user_data.points ? user_data.points: 0,
+                img: user_data.photoURL,
+                level: "The Wise",
+                streak: 2,
+                }])
+            }
+        })
+  },[user]);
 
   return (
     <div
@@ -215,8 +205,17 @@ export default function LeaderBoard() {
           <th>Streak</th>
         </thead>
         <tbody>
-          {data.map((value, index) => (
-            <tr>
+            
+          {data.filter(val => val.points >= period.start && val.points <= period.end).sort((a,b) => b.points - a.points).map((value, index) => (
+            <tr className={
+                
+                user == null ?
+                "bg-white"
+                :
+                value.name === user.displayName ? 
+                "bg-light":"bg-white"
+                
+            }>
               <td>{index + 1}</td>
               <td>
                 <img src={value.img} width="75" alt="" className="image" />
@@ -225,7 +224,7 @@ export default function LeaderBoard() {
                 <h5 className="name text-dark">{value.name}</h5>
               </td>
               <td>
-                <h>{value.score}</h>
+                <h>{value.points}</h>
               </td>
               <td>{value.streak}🔥</td>
             </tr>
