b043059d70cb42622af24f22b97cac6f2bac1ac9
khssupriya <khssupriya@gmail.com>
2022-10-30 07:24:03 +0530

    update actions page

--- src/pages/Actions.js
+++ src/pages/Actions.js
@@ -19,51 +19,51 @@ export default function Actions() {
     
     <div>
 
-        <Toast onClose={() => setShowToast1(false)} show={showToast1} delay={8000} autohide>
+        <Toast onClose={() => setShowToast1(false)} show={showToast1} delay={2000} autohide>
           
           <Toast.Header>
           <img display='rounded me-2' src={require('../assets/gold-coin.gif')} width="80px" height="40px"></img>
             <strong className="me-auto">Points Added</strong>
-            <small>+50</small>
+            <strong>+50</strong>
           </Toast.Header>
           <Toast.Body>Woohoo, you're rewarded for being a great driver!</Toast.Body>
         </Toast>
 
-        <Toast onClose={() => setShowToast2(false)} show={showToast2} delay={8000} autohide>
+        <Toast onClose={() => setShowToast2(false)} show={showToast2} delay={2000} autohide>
           
           <Toast.Header>
           <img display='rounded me-2' src={require('../assets/ohno.png')} width="80px" height="40px"></img>
             <strong className="me-auto">Points Deducted</strong>
-            <small>-20</small>
+            <strong>-20</strong>
           </Toast.Header>
           <Toast.Body>Oh no, you have a penalty for bad driving.!</Toast.Body>
         </Toast>
 
-        <div className="container">
+        <div className="container my-5">
             <div className="row">
-            <div className="col-3"></div>
-            <div className="col-3">
-                <Button onClick={addPoints} variant="primary" className='m-2'>Safe Driving for a week</Button>
+            {/* <div className="col-3"></div> */}
+            <div className="d-grid gap-2 col-3 mx-auto">
+                <Button onClick={addPoints} variant="primary" className='m-2' style={{width: '600'}}>Safe Driving for a week</Button>
             </div>
-            <div className="col-3">
-                <Button onClick={substractPoints} variant="primary" className='m-2'>Cross a red light</Button>
+            <div className="d-grid gap-2 col-3 mx-auto">
+                <Button onClick={substractPoints} variant="primary" className='m-2' style={{width: '600'}}>Cross a red light</Button>
             </div>
             </div>
             <div className="row">
-            <div className="col-3"></div>
-            <div className="col-3">
-            <Button onClick={addPoints} variant="primary" className='m-2'>Regular maintenance</Button>
+            {/* <div className="col-3"></div> */}
+            <div className="d-grid gap-2 col-3 mx-auto">
+            <Button onClick={addPoints} variant="primary" className='m-2' style={{width: '600'}}>Regular maintenance</Button>
             </div>
-            <div className="col-3">
+            <div className="d-grid gap-2 col-3 mx-auto">
             <Button onClick={substractPoints} variant="primary" className='m-2'>Sudden Acceleration</Button>
             </div>
             </div>
             <div className="row">
-            <div className="col-3"></div>
-            <div className="col-3">
+            {/* <div className="col-3"></div> */}
+            <div className="d-grid gap-2 col-3 mx-auto">
             <Button onClick={addPoints} variant="primary" className='m-2'>Wears seatbelt</Button>
             </div>
-            <div className="col-3">
+            <div className="d-grid gap-2 col-3 mx-auto">
             <Button onClick={substractPoints} variant="primary" className='m-2'>Crossing speed limit</Button>
             </div>
             </div>
